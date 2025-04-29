9e7d3c227eaac2f5b5baf73369766814b63d8148
caiosntos <caiogiovanists@outlook.com>
2025-04-28 21:06:28 -0300

    feat:Primeira OT de JavaScript

---
+++ OT1/app.js
@@ -0,0 +1,38 @@
+/*Object: Objeto é tipo uma coleção de dados. Pensa num pacote que tem várias
+coisas dentro, cada coisa com seu nome (vamos nos aprofundar em objetos
+    mais para a frente).*/
+let pessoa = {
+    nome: "Caio Giovani Santos",
+    idade: 22,
+    linguagemFavorita: "Java"
+};
+
+/*Array: Array é uma lista de coisas, que podem ser números, strings, objetos,
+até outros arrays.*/
+let lingaguens = ["JavaScript","TypeScript","Java"];
+
+/*String: Sabe aquele texto, frase, palavra? Isso é uma string. Tudo que fica
+entre aspas, pode ser simples ou duplas*/
+let nome = "Caio";
+let frase = "é foda!"
+
+/*Declaração de números*/
+let idade = 20;
+let pi = 3.14;
+
+/*Boolean: Dois valores true ou false*/
+let amoCodar = true;
+let odeioCodar = false;
+
+/*null: declara que a variavel ta vazia*/
+let semValor = null;
+
+/*Symbol: É um tipo mais raro, usado para criar identificadores únicos. Não é
+tão comum para iniciantes, mas é bom saber que existe.*/
+let id = Symbol('id');
+
+
+let nomeDesafio = "Caio";
+let idadeDesafio = "22";
+let comidaDesafio = "Sushi.";
+console.log("Meu nome é",nomeDesafio,"tenho ",idadeDesafio,"anos e minha comida favorita é",comidaDesafio);
