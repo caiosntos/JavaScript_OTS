dc612d02bec7771c1b281589658a18a6a0731f2c
caiosntos <caiogiovanists@outlook.com>
2025-05-01 17:29:24 -0300

    Feat:Finalizando OT8

---
+++ OT8/app.js
@@ -0,0 +1,25 @@
+let mayvar;
+
+myvar = 10;
+
+let soma = 15+8;
+
+soma += 1;
+
+soma *=3;
+
+console.log(soma);
+
+let uniSenai = true;
+
+let comida = ["Arroz", "Feijão", "Ovo"];
+
+console.log(comida[1]);
+
+console.log(soma === myvar);
+
+function divisao(num1, num2){
+    return num1 / num2;
+}
+
+console.log(divisao(10,2));
