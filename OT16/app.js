9f7185a0c2f273e89caa7a2bbd74859c53287117
caiosntos <caiogiovanists@outlook.com>
2025-05-01 23:49:14 -0300

    feat:Finalizando OT16

--- OT16/app.js
+++ OT16/app.js
@@ -4,9 +4,7 @@ Crie uma variável qualquer, que receba um array com alguns valores
 aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-
 let vetor = [1,2,3,4,5];
-
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
